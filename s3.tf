4139b08da3ceb2655b0a573afa39dc0e9d2fb9b4
wilma <94570156+wilmatumenta@users.noreply.github.com>
2025-10-26 15:59:18 +0000

    environment updated

--- s3.tf
+++ s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "mybucket" {
 
   tags = {
     Name        = "My bucket"
-    Environment = "Dev"
+    Environment = var.environment
   }
 }
 ## specifying the owner of the bucket
